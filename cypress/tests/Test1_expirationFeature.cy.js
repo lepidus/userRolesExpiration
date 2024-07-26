9b1ea613d0dcdca7c073cbe90c138e911300fef7
Jhon <jhon@lepidus.com.br>
2024-07-26 15:48:55 -0400

    Adds integration test - Check role expiration
    Issue: documentacao-e-tarefas/scielo#685
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test1_expirationFeature.cy.js
+++ cypress/tests/Test1_expirationFeature.cy.js
@@ -39,4 +39,37 @@ describe('User Roles Expiration - Expiration feature', function () {
 
         cy.contains('button', 'Save').click();
     });
+    it('Admin expires reviewer role in journal', function () {
+        cy.login('admin', 'admin', 'publicknowledge');
+        cy.contains('a', 'Website').click();
+		cy.waitJQuery();
+		cy.get('#plugins-button').click();
+		
+        cy.contains('span', 'User Roles Expiration').parent().parent().within(() => {
+            cy.get('.show_extras').click();
+        });
+        cy.contains('a', 'Expire role').click();
+        cy.get('select[name="roleSelected"]').select('Reviewer');
+        cy.contains('button', 'Expire').click();
+        cy.wait(2000);
+    });
+    it('Role expiration did not affect other roles, even in another journal', function () {
+        cy.login('jjanssen', null, 'publicknowledge');
+        cy.get('.app__headerActions button').eq(1).click();
+        cy.contains('a', 'Edit Profile').click();
+        
+        cy.get('.ui-tabs-anchor:contains("Roles")').click();
+
+        cy.contains('label', 'Reviewer').within(() => {
+            cy.get('input[name^=reviewerGroup]').should('not.be.checked');
+        });
+        cy.contains('label', 'Author').within(() => {
+            cy.get('input[name^=authorGroup]').should('be.checked');
+        });
+
+        cy.contains('span', 'Register with other journals').parent().click();
+        cy.contains('label', 'Brazilian Journal of Cosmethics').parent().within(() => {
+            cy.get('input[name^=reviewerGroup]').should('be.checked');
+        });
+    });
 });
